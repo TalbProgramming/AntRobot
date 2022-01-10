6759f49fb2017c2ef988d4ece1ecf1378a6a5f9c
talB <65766191+TalbProgramming@users.noreply.github.com>
2022-01-10 17:52:21 +0200

    Add files via upload

--- controllerCode.ino
+++ controllerCode.ino
@@ -65,8 +65,8 @@ data.right = 1;
 data.eButton = 1;
 data.fButton = 1;
 data.JoyStickButton = 1;
-data.JoyStickX = 508;
-data.JoyStickY = 509;
+data.JoyStickX = 512;
+data.JoyStickY = 512;
 // ~~~~~~~~~~~~~~ \\ 
 //===============================================================================
 //  Initialization
@@ -96,6 +96,7 @@ void loop() {
   delay(250);
 
   // put the inputs in the data variable to be initialized
+  // will be 0 if the button is pressed
   data.up = digitalRead(UP_BTN);
   data.down = digitalRead(DOWN_BTN);
   data.left = digitalRead(LEFT_BTN);
@@ -103,11 +104,79 @@ void loop() {
   data.eButton = digitalRead(E_BTN);
   data.fButton = digitalRead(F_BTN);
   data.JoyStickButton = digitalRead(JOYSTICK_BTN);
-  data.JoyStickX = analogRead(JOYSTICK_AXIS_X);
-  data.JoyStickY = analogRead(JOYSTICK_AXIS_Y);
+  data.JoyStickX = analogRead(JOYSTICK_AXIS_X); // reads between 0 - 1023
+                                                // the default is 512
+  data.JoyStickY = analogRead(JOYSTICK_AXIS_Y); // reads between 0 - 1023
 
-  
+
+  // ~~~~~~~~~~~~~~~~~~~~~ Data initialization ~~~~~~~~~~~~~~~~~~~~~~~~~~ \\
+  // the data will be initalized in the controller to not overload the arduino in the robot.
+
+  int command; 
+  // command variable value - command
+  // 0 = Stretch, 1 = Crouch, 2 = defaultPosition, 3 = inc speed, 4 = dec speed,
+  // 5 = forward, 6 = backward, 7 = right, 8 = left
+
+  // not very efficient 
+
+  if((data.up + data.down + data.left + data.right + data.eButton + data.fButton) < 5)
+  { // if a button was pressed
+    if(data.up == 0)
+    {
+      command = 0;
+    }
+      else if(data.down == 0)
+      {
+        command = 1;
+      }
+        else if(data.left == 0)
+        {
+          command = 2;       
+        }
+          else if(data.eButton == 0)
+          {
+            command = 3;
+          }
+            else if(data.fButton == 0)
+            {
+              command = 4;
+            }
+  }
+    else // if none of the buttons were pressed 
+    {
+      // check for the joyStick
+      continue 
+      if(abs(data.JoyStickX - 512) > abs(JoyStickY - 512))
+      { // the x moved more then the y 
+        if(data.JoyStickX < 450)
+        { // move left
+          command = 8;
+        }
+        else if(data.JoyStickX > 580)
+        { // move right
+          command = 7;
+        }
+      }
+      else
+      {
+         if(data.JoyStickY < 450)
+        { // move backward
+          command = 6;
+        }
+        else if(data.JoyStickY > 580)
+        { // move forward
+          command = 5;
+        }     
+      }      
+    }
+
+
+
+
+
+
+   // ~~~ End of data initialization ~~~ \\ 
   // Send the whole data from the structure to the receiver
-  radio.write(&Data, sizeof(data));
+  radio.write(&data, sizeof(Data));
   delay(250); 
  }
